58a9251b6113b9436a9c281603198829a58ab853
edson.villacorta <edson26web@gmail.com>
2026-01-30 15:33:26 -0400

    Added Presentation controls and command controls

---
+++ ControlsDemo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ControlsDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new CommandsControlsDemo());
+        }
+    }
+}
---
+++ ControlsDemo/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ControlsDemo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ControlsDemo.Pages")]
